dd038125fe50bc6b5533c046a0eed260401c7749
ispadilha <israel.padilha@gmail.com>
2025-05-06 09:59:57 -0300

    Logging chosen simulation

--- src/rest/study/study.ts
+++ src/rest/study/study.ts
@@ -1,14 +1,14 @@
-import express, { Express, Request, Response } from 'express'
-import { InvestmentStudyForm } from '../../class/InvestmentStudy'
-import { PdfField, PdfHandler } from '../../class/PdfHandler'
+import express, { Express, Request, Response } from "express"
+import { InvestmentStudyForm } from "../../class/InvestmentStudy"
+import { PdfField, PdfHandler } from "../../class/PdfHandler"
 const router = express.Router()
 
 router.post("/generate", async (request: Request, response: Response) => {
     const data = request.body as InvestmentStudyForm
 
     try {
-        const values = {...data.confinementStudyForm, ...data.financialAnalysisForm}
-        const fields:PdfField[] = []
+        const values = { ...data.confinementStudyForm, ...data.financialAnalysisForm }
+        const fields: PdfField[] = []
 
         Object.entries(values).forEach(([key, value]) => {
             const field: PdfField = { name: key, value: String(value) }
@@ -21,11 +21,13 @@ router.post("/generate", async (request: Request, response: Response) => {
         const output_dir = "static/relatorios/"
         const fullpath = output_dir + filename
         const chosenSimulation = data.simulation
+        console.log({ chosenSimulation })
 
         const pdf = new PdfHandler({ fields, template_path: `static/template/template_${chosenSimulation}.pdf`, output_dir, filename })
 
         await pdf.fillForm()
 
+        // response.json({ url: `http://192.168.18.134:8090/${fullpath}` })
         response.json({ url: `https://api.connectpec.boz.app.br/${fullpath}` })
     } catch (error) {
         console.log(error)
